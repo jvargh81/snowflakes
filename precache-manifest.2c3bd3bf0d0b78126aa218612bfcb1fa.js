77561df1e12f945888d44ecd06a6d76dec687bdc
Jerrin Joe Varghese <jvargh81@lakers.mercyhurst.edu>
2018-12-11 22:10:49 -0500

    Updates

---
+++ precache-manifest.2c3bd3bf0d0b78126aa218612bfcb1fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "40d730c65cb9ec45dc62",
+    "url": "/jvargh81/snowflakes/static/js/runtime~main.40d730c6.js"
+  },
+  {
+    "revision": "b5a6adc20c847154c405",
+    "url": "/jvargh81/snowflakes/static/js/main.b5a6adc2.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/jvargh81/snowflakes/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "b5a6adc20c847154c405",
+    "url": "/jvargh81/snowflakes/static/css/main.c5da530f.chunk.css"
+  },
+  {
+    "revision": "f454e543f50395248a69f2f17a5a81a5",
+    "url": "/jvargh81/snowflakes/index.html"
+  }
+];
